2abc2ba47dcaf84974d862ff0e93922c334b9c9a
Vincent Boulanger <vboulang@c1r6p7.42quebec.com>
2023-12-09 16:14:42 -0500

    working makefiles

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: vboulang <vboulang@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 14:34:12 by vboulang          #+#    #+#             */
-/*   Updated: 2023/12/01 16:49:49 by vboulang         ###   ########.fr       */
+/*   Updated: 2023/12/06 14:05:14 by vboulang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ int	main(int argc, char **argv)
 {
 	if (argc == 5)
 	{
-		if(access(argv[1], F_OK) != 0)
-			return (0);
-		
-		(void)argv;
+		if (access(argv[1], R_OK) == -1)
+			return (0); //TODO Make error message
+
 		//TODO check/open files?
 		//TODO open infile
 		//TODO execute first command reading from infile and output to pipe
