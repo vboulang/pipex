aec92082f1d67acc070aa74c852e83cf15c31a45
Vincent Boulanger <vboulang@c1r6p7.42quebec.com>
2023-12-12 14:27:03 -0500

    main almost written

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: vboulang <vboulang@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 14:34:12 by vboulang          #+#    #+#             */
-/*   Updated: 2023/12/06 14:05:14 by vboulang         ###   ########.fr       */
+/*   Updated: 2023/12/12 14:13:29 by vboulang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,30 @@
 
 int	main(int argc, char **argv)
 {
+	int	fd_file;
+	int	fd[2];
+	int	id;
+
 	if (argc == 5)
 	{
 		if (access(argv[1], R_OK) == -1)
 			return (0); //TODO Make error message
-
-		//TODO check/open files?
-		//TODO open infile
-		//TODO execute first command reading from infile and output to pipe
-			//TODO execute done in a child with execve
-		//TODO execute second command from pipe to outfile
+		pipe(fd);
+		id = fork();
+		if (id == -1)
+			return (0); //TODO Make error message
+		if (id == 0)
+		{
+			fd_file = open(argv[1], O_RDONLY);
+			if (fd_file == -1)
+				return (0); //TODO Make error message
+			if (dup2(fd[1], STDOUT_FILENO) == -1)
+				return (0); //TODO Make error message
+			if (execve() == -1)
+				return (0); //TODO Make an error message
+		}
 	}
 	else
-		return (0);
+		return (0); //TODO Make error message
 	return (0);
 }
