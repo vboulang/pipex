946b3ac7fb06e8bfd9ada41ef31279078b17a182
Vincent Boulanger <vboulang@c1r6p6.42quebec.com>
2023-12-15 16:40:09 -0500

    added structure and code is working but not giving right result

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: vboulang <vboulang@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 14:34:08 by vboulang          #+#    #+#             */
-/*   Updated: 2023/12/14 18:20:34 by vboulang         ###   ########.fr       */
+/*   Updated: 2023/12/15 15:56:40 by vboulang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 # include <stdio.h>
 # include <string.h>
 # include <sys/wait.h>
+# include "../libft/libft.h"
+
+typedef struct s_cmd
+{
+	char	*path;
+	char	**cmd;
+	int		pnb;
+} t_cmd;
 
 int	main(int argc, char **argv, char **envp);
 
--- src/pipex.c
+++ src/pipex.c
@@ -6,17 +6,21 @@
 /*   By: vboulang <vboulang@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 14:34:12 by vboulang          #+#    #+#             */
-/*   Updated: 2023/12/14 18:52:10 by vboulang         ###   ########.fr       */
+/*   Updated: 2023/12/15 16:24:06 by vboulang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
 
+
 char	*get_path(char **envp, char *str)
 {
 	int	i;
 	int	path_not_found;
-	
+	char	**paths_to_split;
+	char	**paths;
+	char	*correct_path;
+
 	i = 0;
 	path_not_found = 1;
 	while (envp[i] && path_not_found)
@@ -26,7 +30,42 @@ char	*get_path(char **envp, char *str)
 		else
 			path_not_found = 0;
 	}
-	
+	paths_to_split = ft_split(envp[i], '=');
+	paths = ft_split(paths_to_split[1], ':');
+	i = 0;
+	while (paths[i])
+	{
+		correct_path = ft_strjoin(paths[i], "/");
+		correct_path = ft_strjoin(correct_path, str);
+		if(access(correct_path, X_OK) == 0)
+			return (correct_path);
+		else
+			free(correct_path);
+		i++;
+		//TODO test correct path with access X_OK. Si ok, return correct_path (free avant ce qu<on utilise pas)
+	}
+	free(correct_path);
+	return(NULL);//TODO message incorrect command
+}
+
+int	dupfct(int *fd, int fd_file, t_cmd cmd)
+{
+	if (cmd.pnb == 0)
+	{
+
+		if (dup2(fd[1], STDOUT_FILENO) == -1)
+			return (0); //TODO Make error message
+		if (dup2(fd_file, STDIN_FILENO) == -1)
+			return (0); //TODO Make error message
+	}
+	else
+	{
+		if (dup2(fd_file, STDOUT_FILENO) == -1)
+			return (0); //TODO Make error message
+		if (dup2(fd[0], STDIN_FILENO) == -1)
+			return (0); //TODO Make error message
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -34,60 +73,41 @@ int	main(int argc, char **argv, char **envp)
 	int		fd_file;
 	int		fd[2];
 	int		id;
-	char	*path;
-	char	**cmd;
+	t_cmd	cmd;
 
 	if (argc == 5)
 	{
+		cmd.pnb = 0;
 		if (access(argv[1], R_OK) == -1)
 			return (0); //TODO Make error message
 		pipe(fd);
-
-		//START LOOP
-		//while()
-		id = fork();
-		if (id == -1)
-			return (0); //TODO Make error message
-		if (id == 0)
+		while(cmd.pnb < argc-3)
 		{
-			fd_file = open(argv[1], O_RDONLY);
-			if (fd_file == -1)
+			id = fork();
+			if (id == -1)
 				return (0); //TODO Make error message
-			if (dup2(fd[1], STDOUT_FILENO) == -1)
-				return (0); //TODO Make error message
-			if (dup2(fd_file, STDIN_FILENO) == -1)
-				return (0); //TODO Make error message
-			close(fd[0]);
-			close(fd[1]);
-			close(fd_file);
-			path = get_path(envp, argv[2]); //TODO check if good path, access X_OK
-			cmd = ft_split(argv[2], ' ');
-			if (execve(path, cmd, NULL) == -1)
-				return (0); //TODO Make an error message
-		}
-		//END LOOP
-		id = fork();
-		if (id == -1)
-			return (0); //TODO Make error message
-		if (id == 0)
-		{
-			fd_file = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC);
-			if (fd_file == -1)
-				return (0); //TODO Make error message
-			if (dup2(fd_file, STDOUT_FILENO) == -1)
-				return (0); //TODO Make error message
-			if (dup2(fd[0], STDIN_FILENO) == -1)
-				return (0); //TODO Make error message
-			close(fd[0]);
-			close(fd[1]);
-			close(fd_file);
-			path = get_path(envp, argv[2]); //TODO check if good path, access X_OK
-			cmd = ft_split(argv[2], ' ');
-			if (execve(path, cmd, NULL) == -1)
-				return (0); //TODO Make an error message
+			if (id == 0)
+			{
+				if (cmd.pnb == 0)
+					fd_file = open(argv[1], O_RDONLY);
+				else
+					fd_file = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+				if (fd_file == -1)
+					return (0); //TODO Make error message
+				if (dupfct(fd, fd_file, cmd) == -1)
+					return (0); //TODO Make error message and change if comparison to match error returned
+				close(fd[0]);
+				close(fd[1]);
+				close(fd_file);
+				cmd.cmd = ft_split(argv[2], ' ');
+				cmd.path = get_path(envp, cmd.cmd[0]); //TODO check if good path, access X_OK
+				if (execve(cmd.path, cmd.cmd, NULL) == -1)
+					return (0); //TODO Make an error message
+			}
+			cmd.pnb += 1;
 		}
 	}
 	else
-		return (0); //TODO Make error message
+		return (printf("Nope\n")); //TODO Make error message
 	return (0);
 }
