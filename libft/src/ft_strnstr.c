1e05ff184bdfaf6c0ef354ebdbe667069ac93799
Vincent Boulanger <vboulang@c2r2p9.42quebec.com>
2024-01-16 16:56:29 -0500

    ft_strnstr.c to norm

--- libft/src/ft_strnstr.c
+++ libft/src/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: vboulang <vboulang@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 13:24:54 by vboulang          #+#    #+#             */
-/*   Updated: 2023/12/20 14:17:19 by vboulang         ###   ########.fr       */
+/*   Updated: 2024/01/16 16:55:30 by vboulang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,3 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 	}
 	return (NULL);
 }
-/*
-#include <stdio.h>
-#include <string.h>
-
-int main(int ac, char **av)
-{
-	if (ac == 3)
-		printf("%s\n%s\n", strnstr(av[1], av[2], 10), ft_strnstr(av[1], av[2], 10));
-}*/
